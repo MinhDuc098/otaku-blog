8b05f41e1e5c2584a2d867bb52e46e579ba4c299
Đức Nguyễn <86830154+MinhDuc098@users.noreply.github.com>
2025-09-21 03:06:23 +0700

    fix can't build

--- src/main/java/com/example/demo/repositories/NotificationRepository.java
+++ src/main/java/com/example/demo/repositories/NotificationRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.criteria.CriteriaBuilder;
+//import javax.persistence.criteria.CriteriaBuilder;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Integer> {
